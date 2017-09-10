7b2bef4e1b3f6371ca661879c575cf91a70cc95a
yang-zhang <zhangyangcolostate@gmail.com>
2017-09-09 20:23:02 -0400

    Auto find dirs

--- build.py
+++ build.py
@@ -1,8 +1,11 @@
 import subprocess
+import os
 
 
 def main():
-    for folder in ["base", "ds", "kaggle", "r"]:
+    images = [name for name in os.listdir('dockerfiles') if
+              os.path.isdir(os.path.join('dockerfiles', name))]
+    for folder in images:
         subprocess.call(["docker", "build", "dockerfiles/" + folder, "--tag",
                         folder])
 
