2b0cfbc6cec31ac5147b1e166ef1306d82742e59
yang-zhang <zhangyangcolostate@gmail.com>
2017-09-21 16:02:50 -0400

    Add sh

---
+++ docker-setup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Add to ~/.bash_profile:
+# sh "path to docker-setup.sh"
+
+dkrun() {
+  if [ "$1" == "kaggle" ]; then
+    docker run \
+    --rm \
+    -it \
+    -p 8888:8888 \
+    -v $PWD:/opt/notebooks \
+    $1 \
+    /bin/bash -c "/opt/conda/bin/jupyter notebook \
+    --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser \
+    "
+  elif [ "$1" == "r" ]; then
+    docker run \
+    --rm \
+    -v $PWD:/home/rstudio \
+    -p 8787:8787 \
+    -e ROOT=TRUE \
+    r
+  else
+    docker run \
+    --rm \
+    -it \
+    -p 8888:8888 \
+    -v $PWD:/opt/notebooks \
+    $1 \
+    /bin/bash -c "/opt/conda/bin/conda install jupyter -y --quiet && \
+    /opt/conda/bin/jupyter notebook \
+    --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser \
+    --allow-root
+    "
+  fi
+}
