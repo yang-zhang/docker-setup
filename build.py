46f7458a839945e77ea5d8426ebb61b8bb8ca5dd
yang-zhang <zhangyangcolostate@gmail.com>
2017-09-22 19:53:02 -0400

    Regor

--- build.py
+++ build.py
@@ -6,8 +6,10 @@ def main():
     images = [name for name in os.listdir('dockerfiles') if
               os.path.isdir(os.path.join('dockerfiles', name))]
     for folder in images:
-        subprocess.call(["docker", "build", "dockerfiles/" + folder, "--tag",
-                        folder])
+        os.system("docker build -f dockerfiles/" + folder +
+                    "/Dockerfile . --tag " + folder)
+        # subprocess.call(["docker", "build", "-f", "dockerfiles/" + folder,
+        #                 "/Dockerfile", ".", "--tag", folder])
 
 if __name__ == "__main__":
     main()
